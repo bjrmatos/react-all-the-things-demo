e2aa29a32d37c420d222e8f6aa610dd7d8e67a54
BJR Matos <bjrmatos@gmail.com>
2015-07-05 23:10:45 -0500

    [add] data por defecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 'use strict';
 
 var React = require('react'),
-    FilterableProductTable = require('FilterableProductTable');
+    FilterableProductTable = require('./FilterableProductTable.jsx');
 
-React.render(<FilterableProductTable />, document.getElementById('app'));
+var PRODUCTS = [
+  { category: 'Sporting Goods', price: 'S/.49.99', stocked: true, name: 'Football' },
+  { category: 'Sporting Goods', price: 'S/.9.99', stocked: true, name: 'Baseball' },
+  { category: 'Sporting Goods', price: 'S/.29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronics', price: 'S/.4999.99', stocked: true, name: 'TV 50" - Tottus' },
+  { category: 'Electronics', price: 'S/.99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronics', price: 'S/.399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Electronics', price: 'S/.199.99', stocked: true, name: 'Nexus 7' }
+];
+
+React.render(<FilterableProductTable products={PRODUCTS} />, document.getElementById('app'));
